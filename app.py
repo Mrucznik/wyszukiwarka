1e40003dfa9f3fd274189531d298635192a3e884
Mrucznik <mrucznix@gmail.com>
2018-06-05 15:42:54 +0200

    Pobieranie książki

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import *
-
+import urllib3
 app = Flask(__name__)
+
+http = urllib3.PoolManager()
 
 
 @app.route('/')
-def hello_world():
+def index():
     try:
         return render_template("index.html")
     except Exception as e:
         return "Błąd"
+
+
+@app.route('/book/<int:book_id>')
+def book(book_id):
+    r = http.request('GET', 'www.gutenberg.org/files/{0}/{0}.txt'.format(book_id))
+    return r.data
 
 
 if __name__ == '__main__':
